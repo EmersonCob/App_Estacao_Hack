0dbbcabda1de94fe179b20dc2f2057ba7d12f6c4
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-11-15 19:58:43 -0300

    update

--- app/src/main/java/com/jebcosta/appestacao_hack/SplashActivity.kt
+++ app/src/main/java/com/jebcosta/appestacao_hack/SplashActivity.kt
@@ -13,11 +13,15 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         // Abrir a LoginActivity após 3 segundos
+        // Executa a instruções após determinado tempo
         Handler(Looper.getMainLooper()).postDelayed({
             // Iniciar uma intent - transição da tela Splash para a tela Login
+            // Uma das utilidades da classe Intent é usar para abrir uma activity
             val mIntent = Intent(this, LoginActivity::class.java)
+            // Método responsável por executar o Intent
             startActivity(mIntent)
+            // Remove a tela da pilha
             finish()
-        },3000)
+        },3000)// Tempo em milisegundos
     }
 }
