c14fd128532c68f3d3486d06719e9d59b778f0ca
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-11-16 21:44:05 -0300

    update

--- app/src/main/java/com/jebcosta/appestacao_hack/CadastroActivity.kt
+++ app/src/main/java/com/jebcosta/appestacao_hack/CadastroActivity.kt
@@ -2,10 +2,39 @@ package com.jebcosta.appestacao_hack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jebcosta.appestacao_hack.databinding.ActivityCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Criando a lista de opções para o Spinner
+        val listaContinentes = arrayListOf("Continente", "Africa", "Antártida", "America",
+            "Ásia", "Europa", "Oceania")
+
+        val spinnerAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, // Layout
+            listaContinentes        // Dados
+        )
+        // Plugando o adaptador no Spinner da Activity
+        binding.spnCadastroContinente.adapter = spinnerAdapter
+
+        // Quando o botão for clicado faça
+        binding.btnCadastroCadastrar.setOnClickListener {
+        // Recuperar os dados digitados e atribuir as variáveis imutáveis
+
+            val nome = binding.edtCadastroNome.text.toString().trim()
+            val sobrenome = binding.edtCadastroSobrenome.text.toString().trim()
+            val email = binding.edtCadastroEmail.text.toString().trim().lowercase()
+            val senha = binding.edtCadastroSenha.text.toString().trim()
+            val continente = binding.spnCadastroContinente
+
+        }
     }
 }
--- app/src/main/java/com/jebcosta/appestacao_hack/LoginActivity.kt
+++ app/src/main/java/com/jebcosta/appestacao_hack/LoginActivity.kt
@@ -1,11 +1,58 @@
 package com.jebcosta.appestacao_hack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.jebcosta.appestacao_hack.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Quando o botão for clicado, faça
+        binding.btnLoginEntrar.setOnClickListener{
+            // Aqui os dados digitados são capturados e salvos em variáveis imutáveis
+            val email = binding.editLoginEmail.text.toString().trim().lowercase()
+            val senha = binding.editLoginSenha.text.toString().trim()
+
+            // Aqui é feita a validação dos campos
+            if(email.isEmpty()){
+                binding.editLoginEmail.error = "Campo Obrigatório"
+                binding.editLoginEmail.requestFocus()
+            } else if(senha.isEmpty()){
+                binding.editLoginSenha.error = "Campo Obrigatório"
+                binding.editLoginSenha.requestFocus()
+            } else {
+                // Aqui será validado se o e-mail e senha batem
+                if(email == "teste@teste.com" && senha == "123456"){
+                    // Se tudo estiver certo uma mensagem de sucesso será exibida
+                    Toast.makeText(this, "Usuário Logado", Toast.LENGTH_LONG).show()
+
+                    // Em seguinda, a MainActivity será aberta
+                    val mIntent = Intent(this, MainActivity::class.java)
+                    startActivity(mIntent)
+
+                    // Então eliminamos essa activity da pilha.
+                    finish()
+                } else {
+                    // se o E-mail e senha não baterem, então uma mensagem de erro será exibida
+                    Toast.makeText(this, "E-mail ou senha inválidos", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+        // Quando o botão cadastrar for clicado, faça
+        binding.btnLoginCadastrar.setOnClickListener {
+            // A Cadastro Activity deve ser aberta
+            val mIntent = Intent(this, CadastroActivity::class.java)
+            startActivity(mIntent)
+
+        }
+
     }
 }
