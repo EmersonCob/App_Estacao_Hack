aa658f0901b2ea9aedd3a8ea50a0c88556e4bd50
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-11-17 22:58:31 -0300

    update

--- app/src/main/java/com/jebcosta/appestacao_hack/WebActivity.kt
+++ app/src/main/java/com/jebcosta/appestacao_hack/WebActivity.kt
@@ -2,10 +2,25 @@ package com.jebcosta.appestacao_hack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import com.jebcosta.appestacao_hack.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web)
+        binding = ActivityWebBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Habilitar a execução de código javascript
+
+        binding.wbvWeb.settings.javaScriptEnabled = true
+
+
+        // Definir a página Inicial
+        binding.wbvWeb.loadUrl("https://br.cellep.com/estacaohack")
+
+        // definir o WebView como client Web Padrão
+        binding.wbvWeb.webViewClient = WebViewClient()
     }
 }
